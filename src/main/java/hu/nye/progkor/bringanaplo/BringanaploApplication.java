c3ffe23acd39a74d1f828ef32cb95456fc27ef93
Gergo20051223 <kadargerikee@gmail.com>
2026-04-13 12:24:48 +0200

    feat: initial spring boot setup

---
+++ src/main/java/hu/nye/progkor/bringanaplo/BringanaploApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.bringanaplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BringanaploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BringanaploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/progkor/bringanaplo/BringanaploApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.bringanaplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BringanaploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
